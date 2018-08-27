6103a9abc407a80bdf698f063fdd543e5f321aa7
Nicolas Marshall <marshall.nicolas@gmail.com>
2018-08-27 23:53:21 +0200

    add authenticateUserOptions and loginHint

--- noserver-gphotos/no-server.go
+++ noserver-gphotos/no-server.go
@@ -2,6 +2,7 @@ package gphotos
 
 import (
 	"context"
+	"net/url"
 
 	multierror "github.com/hashicorp/go-multierror"
 	gphotos "github.com/nmrshll/google-photos-api-client-go/lib-gphotos"
@@ -11,7 +12,6 @@ import (
 )
 
 type AuthorizedClient oauth2ns.AuthorizedClient
-
 type ClientConstructorOption func() (*AuthorizedClient, error)
 
 func NewClient(options ...ClientConstructorOption) (client *Client, merr error) {
@@ -37,18 +37,38 @@ func NewClient(options ...ClientConstructorOption) (client *Client, merr error) 
 	return nil, stacktrace.Propagate(merr, "all options failed with errors:")
 }
 
-// func FromOAuthCredentials(apiAppCredentials gphotos.APIAppCredentials) ClientConstructorOption {
-// 	return func() (*oauth2ns.AuthorizedClient, error) {
-// 		oauthConfig := gphotos.NewOAuthConfig(apiAppCredentials)
-// 		photosClient := oauth2ns.Authorize(&oauthConfig)
-// 		return photosClient, nil
-// 	}
-// }
+type AuthenticateUserOption func(*AuthenticateUserFuncConfig) error
+type AuthenticateUserFuncConfig struct {
+	userLoginHint string
+}
+
+func WithUserLoginHint(loginHint string) AuthenticateUserOption {
+	return func(config *AuthenticateUserFuncConfig) error {
+		config.userLoginHint = loginHint
+		return nil
+	}
+}
 
 // AuthenticateUser() option creates a new http.Client with a bearer access token
-func AuthenticateUser(oauthConfig *oauth2.Config) ClientConstructorOption {
+func AuthenticateUser(oauthConfig *oauth2.Config, options ...AuthenticateUserOption) ClientConstructorOption {
+	var funcConfig AuthenticateUserFuncConfig
+	for _, optionFunc := range options {
+		optionFunc(&funcConfig)
+	}
+
+	// apply oauth2ns options
+	var oauth2nsOptions []oauth2ns.AuthenticateUserOption
+	if funcConfig.userLoginHint != "" {
+		var urlValues = url.Values{}
+		urlValues.Set("login_hint", funcConfig.userLoginHint)
+		oauth2nsOptions = append(oauth2nsOptions, oauth2ns.WithAuthCallHTTPParams(urlValues))
+	}
+
 	return func() (*AuthorizedClient, error) {
-		authorizedClient := oauth2ns.Authorize(oauthConfig)
+		authorizedClient, err := oauth2ns.AuthenticateUser(oauthConfig, oauth2nsOptions...)
+		if err != nil {
+			return nil, stacktrace.Propagate(err, "failed authenticating user")
+		}
 		return (*AuthorizedClient)(authorizedClient), nil
 	}
 }
@@ -61,12 +81,3 @@ func FromToken(oauthConfig *oauth2.Config, token *oauth2.Token) ClientConstructo
 		}, nil
 	}
 }
-
-// func NewOAuthClient(oauthCreds) (*oauth2ns.AuthorizedClient, error) {
-// 	photosClient := oauth2ns.Authorize(&OAuthConfig)
-// 	return photosClient, nil
-// }
-
-// func NewOauthClientFromToken(token *oauth2.Token) *http.Client {
-// 	return OAuthConfig.Client(context.Background(), token)
-// }
