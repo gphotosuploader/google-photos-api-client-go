54e33c88aab27c60298418c5d12043e3b7fcda3b
Patrick Decat <pdecat@gmail.com>
2019-07-28 14:00:58 +0200

    Maintain backward compatible versions of UploadFile() and GetUploadToken() methods, introduce new UploadFileResumable() and getUploadTokenResumable() methods

--- lib-gphotos/client.go
+++ lib-gphotos/client.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 	"time"
 
@@ -56,11 +57,111 @@ func NewClient(oauthHTTPClient *http.Client, maybeToken ...*oauth2.Token) (*Clie
 }
 
 // GetUploadToken sends the media and returns the UploadToken.
+func (c *Client) GetUploadToken(r io.Reader, filename string) (token string, err error) {
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s/uploads", basePath, apiVersion), r)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/octet-stream")
+	req.Header.Add("X-Goog-Upload-File-Name", filename)
+	req.Header.Set("X-Goog-Upload-Protocol", "raw")
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	uploadToken := string(b)
+	return uploadToken, nil
+}
+
+// UploadFile actually uploads the media and activates it on google photos
+func (c *Client) UploadFile(filePath string, pAlbumID ...string) (*photoslibrary.MediaItem, error) {
+	// validate parameters
+	if len(pAlbumID) > 1 {
+		return nil, stacktrace.NewError("parameters can't include more than one albumID'")
+	}
+	var albumID string
+	if len(pAlbumID) == 1 {
+		albumID = pAlbumID[0]
+	}
+
+	filename := path.Base(filePath)
+	log.Printf("Uploading %s", filename)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "failed opening file")
+	}
+	defer file.Close()
+
+	uploadToken, err := c.GetUploadToken(file, filename)
+	if err != nil {
+		return nil, stacktrace.Propagate(err, "failed getting uploadToken for %s", filename)
+	}
+
+	retry := true
+	retryCount := 0
+	for retry {
+		// TODO: Refactor how retries are done. We should add exponential backoff
+		// https://developers.google.com/photos/library/guides/best-practices#retrying-failed-requests
+		retry = false //nolint
+		batchResponse, err := c.MediaItems.BatchCreate(&photoslibrary.BatchCreateMediaItemsRequest{
+			AlbumId: albumID,
+			NewMediaItems: []*photoslibrary.NewMediaItem{
+				{
+					Description:     filename,
+					SimpleMediaItem: &photoslibrary.SimpleMediaItem{UploadToken: uploadToken},
+				},
+			},
+		}).Do()
+		if err != nil {
+			// handle rate limit error by sleeping and retrying
+			if err.(*googleapi.Error).Code == 429 {
+				after, err := strconv.ParseInt(err.(*googleapi.Error).Header.Get("Retry-After"), 10, 64)
+				if err != nil || after == 0 {
+					after = 10
+				}
+				log.Printf("Rate limit reached, sleeping for %d seconds...", after)
+				time.Sleep(time.Duration(after) * time.Second)
+				retry = true
+				continue
+			} else if retryCount < 3 {
+				log.Printf("Error during upload, sleeping for 10 seconds before retrying...")
+				time.Sleep(10 * time.Second)
+				retry = true
+				retryCount++
+				continue
+			}
+			return nil, stacktrace.Propagate(err, "failed adding media %s", filename)
+		}
+
+		if batchResponse == nil || len(batchResponse.NewMediaItemResults) != 1 {
+			return nil, stacktrace.NewError("len(batchResults) should be 1")
+		}
+		result := batchResponse.NewMediaItemResults[0]
+		if result.Status.Message != "OK" && result.Status.Message != "Success" {
+			// TODO: We should use a different field like `googleapi.ServerResponse`
+			return nil, stacktrace.NewError("status message should be OK/Succecc, found: %s", result.Status.Message)
+		}
+
+		log.Printf("%s uploaded successfully as %s", filename, result.MediaItem.Id)
+		return result.MediaItem, nil
+	}
+	return nil, nil
+}
+
+// getUploadTokenResumable sends the media and returns the UploadToken.
 // The uploadURL parameter will be updated
-func (c *Client) GetUploadToken(r io.ReadSeeker, filename string, fileSize int64, uploadURL *string) (uploadToken string, err error) {
+func (c *Client) getUploadTokenResumable(r io.ReadSeeker, filename string, fileSize int64, uploadURL *string) (uploadToken string, err error) {
 	var offset int64
 
-	if *uploadURL != "" {
+	if uploadURL != nil && *uploadURL != "" {
 		log.Printf("Checking status of upload URL '%s'\n", *uploadURL)
 		// Query previous upload status and get offset if active
 		req, err := http.NewRequest("POST", *uploadURL, nil)
@@ -93,7 +194,7 @@ func (c *Client) GetUploadToken(r io.ReadSeeker, filename string, fileSize int64
 		}
 	}
 
-	if *uploadURL == "" {
+	if uploadURL == nil || *uploadURL == "" {
 		// Get new upload URL
 		log.Printf("Getting new upload URL for '%s'\n", filename)
 		req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s/uploads", basePath, apiVersion), nil)
@@ -144,9 +245,9 @@ func (c *Client) GetUploadToken(r io.ReadSeeker, filename string, fileSize int64
 	return uploadToken, nil
 }
 
-// UploadFile actually uploads the media and activaes it on google photos
+// UploadFileResumable actually uploads the media and activaes it on google photos
 // The uploadURL parameter will be updated
-func (c *Client) UploadFile(filePath string, uploadURL *string, pAlbumID ...string) (*photoslibrary.MediaItem, error) {
+func (c *Client) UploadFileResumable(filePath string, uploadURL *string, pAlbumID ...string) (*photoslibrary.MediaItem, error) {
 	// validate parameters
 	if len(pAlbumID) > 1 {
 		return nil, stacktrace.NewError("parameters can't include more than one albumID'")
@@ -170,7 +271,7 @@ func (c *Client) UploadFile(filePath string, uploadURL *string, pAlbumID ...stri
 	}
 	fileSize := fileStat.Size()
 
-	uploadToken, err := c.GetUploadToken(file, filePath, fileSize, uploadURL)
+	uploadToken, err := c.getUploadTokenResumable(file, filePath, fileSize, uploadURL)
 	if err != nil {
 		return nil, stacktrace.Propagate(err, "failed getting uploadToken for %s", filePath)
 	}
