dd01959874bc1e413c1d17f9a8bb5a668205f000
pacoorozco <paco@pacoorozco.info>
2019-09-21 20:28:23 +0200

    doc: Document DEPRECATED methods
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- lib-gphotos/client.go
+++ lib-gphotos/client.go
@@ -3,9 +3,9 @@ package gphotos
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"golang.org/x/oauth2"
+	"golang.org/x/xerrors"
 
 	"github.com/gphotosuploader/googlemirror/api/photoslibrary/v1"
 )
@@ -15,38 +15,51 @@ import (
 type Client struct {
 	*photoslibrary.Service
 	*http.Client
-	token *oauth2.Token
+
+	token *oauth2.Token // DEPRECATED: `token` will disappear in the next MAJOR version.
 
 	log *log.Logger
 }
 
-// Token returns the value of the token used by the gphotos Client
-// Cannot be used to set the token
-func (c *Client) Token() *oauth2.Token {
-	if c.token == nil {
-		return nil
-	}
-	return &(*c.token)
-}
-
-// NewClient constructs a new PhotosClient from an oauth httpClient
-func NewClient(oauthHTTPClient *http.Client, maybeToken ...*oauth2.Token) (*Client, error) {
+// NewClient constructs a new PhotosClient from an oauth httpClient.
+//
+// `httpClient` is an HTTP Client with authentication credentials.
+//
+// DEPRECATED: `maybeToken` will disappear in the next MAJOR version.
+// This package doesn't need Client.token anymore, used `Client.Client` instead.
+func NewClient(httpClient *http.Client, maybeToken ...*oauth2.Token) (*Client, error) {
 	var token *oauth2.Token
+
+	if httpClient == nil {
+		return nil, xerrors.New("client is nil")
+	}
 
 	if len(maybeToken) > 1 {
 		token = maybeToken[0]
 	}
 
-	photosService, err := photoslibrary.New(oauthHTTPClient)
+	photosService, err := photoslibrary.New(httpClient)
 	if err != nil {
 		return nil, err
 	}
 
 	c := Client{
 		Service: photosService,
-		Client:  oauthHTTPClient,
+		Client:  httpClient,
 		token:   token,
-		log:     log.New(os.Stdout, logPrefix, logFlags),
+		log:     defaultLogger(),
 	}
 	return &c, nil
+}
+
+// Token returns the value of the token used by the gphotos Client
+// Cannot be used to set the token
+//
+// DEPRECATED: This method will disappear in the next MAJOR version.
+// Use the authenticated HTTP Client `Client.Client` instead.
+func (c *Client) Token() *oauth2.Token {
+	if c.token == nil {
+		return nil
+	}
+	return &(*c.token)
 }
