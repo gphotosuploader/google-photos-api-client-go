de2f3b78020282a187b3e4ed6cbe4983ed6777be
pacoorozco <paco@pacoorozco.info>
2019-09-21 19:09:32 +0200

    feat: Add a Logger to the package
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- lib-gphotos/client.go
+++ lib-gphotos/client.go
@@ -1,16 +1,14 @@
 package gphotos
 
 import (
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/palantir/stacktrace"
 	"golang.org/x/oauth2"
 
 	"github.com/gphotosuploader/googlemirror/api/photoslibrary/v1"
 )
-
-const apiVersion = "v1"
-const basePath = "https://photoslibrary.googleapis.com/"
 
 // Client is a client for uploading a media.
 // photoslibrary does not provide `/v1/uploads` API so we implement here.
@@ -18,6 +16,8 @@ type Client struct {
 	*photoslibrary.Service
 	*http.Client
 	token *oauth2.Token
+
+	log *log.Logger
 }
 
 // Token returns the value of the token used by the gphotos Client
@@ -32,17 +32,21 @@ func (c *Client) Token() *oauth2.Token {
 // NewClient constructs a new PhotosClient from an oauth httpClient
 func NewClient(oauthHTTPClient *http.Client, maybeToken ...*oauth2.Token) (*Client, error) {
 	var token *oauth2.Token
-	switch len(maybeToken) {
-	case 0:
-	case 1:
+
+	if len(maybeToken) > 1 {
 		token = maybeToken[0]
-	default:
-		return nil, stacktrace.NewError("NewClient() parameters should have maximum 1 token")
 	}
 
 	photosService, err := photoslibrary.New(oauthHTTPClient)
 	if err != nil {
 		return nil, err
 	}
-	return &Client{photosService, oauthHTTPClient, token}, nil
+
+	c := Client{
+		Service: photosService,
+		Client:  oauthHTTPClient,
+		token:   token,
+		log:     log.New(os.Stdout, logPrefix, logFlags),
+	}
+	return &c, nil
 }
