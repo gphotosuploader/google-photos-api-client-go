bcc5f9d20c89e1044aff45a17b64c9d022a35bba
pacoorozco <paco@pacoorozco.info>
2019-10-23 09:54:46 +0200

    feat: add Logger interface and a DiscardLogger
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ lib-gphotos/internal/log/logger.go
@@ -0,0 +1,28 @@
+package log
+
+// Logger defines the common logging interface
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+
+	Panic(args ...interface{})
+	Panicf(format string, args ...interface{})
+
+	Done(args ...interface{})
+	Donef(format string, args ...interface{})
+
+	Fail(args ...interface{})
+	Failf(format string, args ...interface{})
+}
