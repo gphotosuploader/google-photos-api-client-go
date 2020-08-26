4cd201442138ca8203f8bb4021fc2619ca452621
pacoorozco <paco@pacoorozco.info>
2020-08-26 22:23:49 +0200

    add method documentation
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- internal/photoservice/googlephotos.go
+++ internal/photoservice/googlephotos.go
@@ -1,4 +1,4 @@
-package gphotos
+package photoservice
 
 import (
 	"context"
@@ -11,13 +11,22 @@ import (
 	"github.com/gphotosuploader/google-photos-api-client-go/v2/internal/log"
 )
 
+// GooglePhotosService represents a Google Photos client using the `gphotosuploader/googlemirror`
+// photos library with exponential back-off implementation.
 type GooglePhotosService struct {
 	service *photoslibrary.Service
 	log     log.Logger
 }
 
+// NewGooglePhotosService returns a Google Photos client using the `gphotosuploader/googlemirror`
+// photos library with exponential back-off implementation. It can ve configured with Options.
 func NewGooglePhotosService(httpClient *http.Client, options ...Option) (*GooglePhotosService, error) {
 	logger := defaultLogger()
+
+	s, err := photoslibrary.New(httpClient)
+	if err != nil {
+		return nil, err
+	}
 
 	for _, o := range options {
 		switch o.Name() {
@@ -26,17 +35,14 @@ func NewGooglePhotosService(httpClient *http.Client, options ...Option) (*Google
 		}
 	}
 
-	s, err := photoslibrary.New(httpClient)
-	if err != nil {
-		return nil, err
-	}
-
 	return &GooglePhotosService{
 		service: s,
 		log:     logger,
 	}, nil
 }
 
+// ListAlbums makes a `Albums.List` call to Google Photos API and returns the response.
+// It implements retries using exponential back-off.
 func (s *GooglePhotosService) ListAlbums(ctx context.Context, pageSize int64, pageToken string) (*photoslibrary.ListAlbumsResponse, error) {
 	list := s.service.Albums.List().PageSize(pageSize).PageToken(pageToken)
 	b, cancel := defaultRetryPolicy.Start(ctx)
@@ -58,6 +64,8 @@ func (s *GooglePhotosService) ListAlbums(ctx context.Context, pageSize int64, pa
 	return nil, fmt.Errorf("retry over")
 }
 
+// CreateAlbum makes a `Albums.Create` call to Google Photos API and returns the response.
+// It implements retries using exponential back-off.
 func (s *GooglePhotosService) CreateAlbum(ctx context.Context, request *photoslibrary.CreateAlbumRequest) (*photoslibrary.Album, error) {
 	create := s.service.Albums.Create(request)
 	b, cancel := defaultRetryPolicy.Start(ctx)
@@ -80,6 +88,8 @@ func (s *GooglePhotosService) CreateAlbum(ctx context.Context, request *photosli
 	return nil, fmt.Errorf("retry over")
 }
 
+// CreateMediaItems makes a `MediaItems.BatchCreate` call to Google Photos API and returns the response.
+// It implements retries using exponential back-off.
 func (s *GooglePhotosService) CreateMediaItems(ctx context.Context, request *photoslibrary.BatchCreateMediaItemsRequest) (*photoslibrary.BatchCreateMediaItemsResponse, error) {
 	create := s.service.MediaItems.BatchCreate(request)
 	b, cancel := defaultRetryPolicy.Start(ctx)
