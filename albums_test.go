15c60271816ab44769a93d8a202230277761f325
pacoorozco <paco@pacoorozco.info>
2020-09-10 12:26:30 +0200

    Homogenize test errors
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- albums_test.go
+++ albums_test.go
@@ -19,39 +19,38 @@ func TestClient_FindAlbum(t *testing.T) {
 	ctx := context.Background()
 	c, err := gphotos.NewClient(http.DefaultClient, gphotos.WithPhotoService(mockedService), gphotos.WithCacher(mockedCache))
 	if err != nil {
-		t.Fatalf("error was not expected at this point: err=%v", err)
+		t.Fatalf("error was not expected at this point. err: %s", err)
 	}
 
-	t.Run("WithNonExistentAlbum", func(t *testing.T) {
-		truncateAlbumGallery()
+	t.Run("ReturnsErrAlbumNotFoundIfAlbumDoesNotExist", func(t *testing.T) {
+		feedAlbumGallery(0)
 		_, err := c.FindAlbum(ctx, "nonexistent")
 		if !errors.Is(err, gphotos.ErrAlbumNotFound) {
-			t.Errorf("error was not expected. want: %v, got: %v", gphotos.ErrAlbumNotFound, err)
+			t.Errorf("error was not expected. want: %s, got: %s", gphotos.ErrAlbumNotFound, err)
 		}
 	})
 
-	t.Run("WithCachedAlbum", func(t *testing.T) {
-		truncateAlbumGallery()
-		want := "cached"
-
+	t.Run("ReturnsAlbumIfItIsCached", func(t *testing.T) {
+		feedAlbumGallery(0)
+		want := "cached-album" // It's already on the cache, see mockedCache.
 		got, err := c.FindAlbum(ctx, want)
 		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
+			t.Fatalf("error was not expected at this point. err: %s", err)
 		}
 		if want != got.Title {
-			t.Errorf("want: %v, got: %v", want, got)
+			t.Errorf("want: %s, got: %s", want, got.Title)
 		}
 	})
 
-	t.Run("WithNonCachedAlbum", func(t *testing.T) {
-		initializeAlbumGallery(5)
+	t.Run("ReturnsAlbumEvenItIsNotCached", func(t *testing.T) {
+		feedAlbumGallery(5) // Creates five albums: album-1 to album-5.
 		want := "album-1"
 		got, err := c.FindAlbum(ctx, want)
 		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
+			t.Fatalf("error was not expected at this point. err: %s", err)
 		}
 		if want != got.Title {
-			t.Errorf("want: %v, got: %v", want, got)
+			t.Errorf("want: %s, got: %s", want, got.Title)
 		}
 	})
 }
@@ -60,43 +59,31 @@ func TestClient_ListAlbums(t *testing.T) {
 	ctx := context.Background()
 	c, err := gphotos.NewClient(http.DefaultClient, gphotos.WithPhotoService(mockedService), gphotos.WithCacher(mockedCache))
 	if err != nil {
-		t.Fatalf("error was not expected at this point: err=%v", err)
+		t.Fatalf("error was not expected at this point. err: %s", err)
 	}
 
-	t.Run("WithEmptyAlbumGallery", func(t *testing.T) {
-		truncateAlbumGallery()
-		got, err := c.ListAlbums(ctx)
-		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
-		}
-		if len(got) > 0 {
-			t.Errorf("no albums should be listed. got: %d", len(got))
-		}
-	})
+	var tests = []struct {
+		name string
+		want int
+	}{
+		{name: "Gallery without albums", want: 0},
+		{name: "Gallery with on album", want: 1},
+		{name: "Gallery with five albums", want: 5},
+		{name: "Gallery with fifty albums", want: 50},
+		{name: "Gallery with fifty one albums", want: 51},
+		{name: "Gallery with five hundred albums", want: 500},
+	}
 
-	t.Run("WithSmallAlbumGallery", func(t *testing.T) {
-		want := 5
-		initializeAlbumGallery(want)
+	for _, tc := range tests {
+		feedAlbumGallery(tc.want)
 		got, err := c.ListAlbums(ctx)
 		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
+			t.Fatalf("error was not expected at this point. err: %s", err)
 		}
-		if len(got) != want {
-			t.Errorf("want: %d, got: %d", want, len(got))
+		if len(got) != tc.want {
+			t.Errorf("case: %s, want: %d, got: %d", tc.name, tc.want, len(got))
 		}
-	})
-
-	t.Run("WithLargeAlbumGallery", func(t *testing.T) {
-		want := 500
-		initializeAlbumGallery(want)
-		got, err := c.ListAlbums(ctx)
-		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
-		}
-		if len(got) != want {
-			t.Errorf("want: %d, got: %d", want, len(got))
-		}
-	})
+	}
 
 }
 
@@ -107,12 +94,12 @@ func TestClient_CreateAlbum(t *testing.T) {
 		t.Fatalf("error was not expected at this point: err=%v", err)
 	}
 
-	t.Run("ReturnsExistingAlbum", func(t *testing.T) {
-		initializeAlbumGallery(1)
+	t.Run("ReturnsAnExistingAlbum", func(t *testing.T) {
+		feedAlbumGallery(1) // album-1 has been created on the gallery.
 		want := "album-1"
 		got, err := c.CreateAlbum(ctx, want)
 		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
+			t.Fatalf("error was not expected at this point. err: %s", err)
 		}
 		if got.Title != want {
 			t.Errorf("want: %s, got: %s", want, got.Title)
@@ -120,11 +107,11 @@ func TestClient_CreateAlbum(t *testing.T) {
 	})
 
 	t.Run("ReturnsCreatedAlbum", func(t *testing.T) {
-		truncateAlbumGallery()
+		feedAlbumGallery(0)
 		want := "dummy"
 		got, err := c.CreateAlbum(ctx, want)
 		if err != nil {
-			t.Fatalf("error was not expected at this point. err: %v", err)
+			t.Fatalf("error was not expected at this point. err: %s", err)
 		}
 		if got.Title != want {
 			t.Errorf("want: %s, got: %s", want, got.Title)
@@ -132,7 +119,6 @@ func TestClient_CreateAlbum(t *testing.T) {
 	})
 
 	t.Run("ShouldFailDueToAPIError", func(t *testing.T) {
-		truncateAlbumGallery()
 		want := "should-fail"
 		got, err := c.CreateAlbum(ctx, want)
 		if err == nil {
@@ -145,19 +131,14 @@ func TestClient_CreateAlbum(t *testing.T) {
 // albumGallery represents the Album repository to mock Google Photos calls.
 var albumGallery []*photoslibrary.Album
 
-// initializeAlbumGallery will add the specified number of albums to the Album gallery.
+// feedAlbumGallery will add the specified number of albums to the Album gallery.
 // All the albums follow the template `album-<number>` where `<number>` is an incremental integer.
-func initializeAlbumGallery(n int) {
-	truncateAlbumGallery()
+func feedAlbumGallery(n int) {
+	albumGallery = nil
 	for i := 1; i <= n; i++ {
 		a := photoslibrary.Album{Title: fmt.Sprintf("album-%d", i)}
 		albumGallery = append(albumGallery, &a)
 	}
-}
-
-// truncateAlbumGallery will empty the Album gallery.
-func truncateAlbumGallery() {
-	albumGallery = nil
 }
 
 var mockedService = &mock.PhotoService{
@@ -195,8 +176,8 @@ var mockedService = &mock.PhotoService{
 
 var mockedCache = &mock.Cache{
 	GetAlbumFn: func(ctx context.Context, title string) (album *photoslibrary.Album, err error) {
-		if title == "cached" {
-			return &photoslibrary.Album{Title: "cached"}, nil
+		if title == "cached-album" {
+			return &photoslibrary.Album{Title: "cached-album"}, nil
 		}
 		return nil, cache.ErrCacheMiss
 	},
