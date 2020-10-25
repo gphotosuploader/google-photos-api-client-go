71a3f2c042ce9f0a32c498d06dd96448a809e8a7
pacoorozco <paco@pacoorozco.info>
2020-10-25 08:34:01 +0100

    Fix bug on FileUploadItem Open()
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- fileuploaditem.go
+++ fileuploaditem.go
@@ -12,33 +12,33 @@ type FileUploadItem string
 // Open returns a stream.
 // Caller should close it finally.
 func (m FileUploadItem) Open() (io.ReadSeeker, int64, error) {
-	f, err := os.Stat(m.String())
+	r, err := os.Open(m.path())
 	if err != nil {
 		return nil, 0, err
 	}
-	r, err := os.Open(m.String())
+	fi, err := r.Stat()
 	if err != nil {
 		return nil, 0, err
 	}
-	return r, f.Size(), nil
+	return r, fi.Size(), nil
 }
 
 // Name returns the filename.
 func (m FileUploadItem) Name() string {
-	f, err := os.Stat(m.String())
+	f, err := os.Stat(m.path())
 	if err != nil {
 		return ""
 	}
 	return path.Base(f.Name())
 }
 
-func (m FileUploadItem) String() string {
+func (m FileUploadItem) path() string {
 	return string(m)
 }
 
 // Size returns size of the file.
 func (m FileUploadItem) Size() int64 {
-	f, err := os.Stat(m.String())
+	f, err := os.Stat(m.path())
 	if err != nil {
 		return 0
 	}
