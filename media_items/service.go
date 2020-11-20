ed80248fcbcb0e79ef1d171523af83cad5c18975
pacoorozco <paco@pacoorozco.info>
2020-11-20 10:00:47 +0100

    Add MediaItems service
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- media_items/service.go
+++ media_items/service.go
@@ -2,30 +2,24 @@ package media_items
 
 import (
 	"context"
-	"net/http"
-
-	"github.com/duffpl/google-photos-api-client/albums"
-	"github.com/duffpl/google-photos-api-client/media_items"
-
-	"github.com/gphotosuploader/google-photos-api-client-go/v2/uploader"
 )
 
 // MediaItemsService represents a Google Photos client for media management.
 type MediaItemsService interface {
-	CreateFromFile(ctx context.Context, albumId string, path string) (MediaItem, error)
-	CreateManyFromFiles(ctx context.Context, albumId string, paths []string) ([]MediaItem, error)
-	Get(ctx context.Context, itemId string) (*MediaItem, error)
-	List(ctx context.Context, options *ListOptions, pageToken string) (mediaItems []MediaItem, nextPageToken string, err error)
-	Update(ctx context.Context, mediaItem MediaItem, updateMask []Field) (*MediaItem, error)
+	Create(ctx context.Context, mediaItem SimpleMediaItem) (MediaItem, error)
+	CreateMany(ctx context.Context, mediaItems []SimpleMediaItem) ([]MediaItem, error)
+	CreateToAlbum(ctx context.Context, albumId string, mediaItem SimpleMediaItem) (MediaItem, error)
+	CreateManyToAlbum(ctx context.Context, albumId string, mediaItems []SimpleMediaItem) ([]MediaItem, error)
+	Get(ctx context.Context, mediaItemId string) (*MediaItem, error)
+	ListByAlbum(ctx context.Context, albumId string) ([]MediaItem, error)
 }
 
-// Repository represents the Google Photos API client for media management.
+// Repository represents a media items repository.
 type Repository interface {
-	BatchCreateItemsFromFiles(albumId string, paths []string, position albums.AlbumPosition, ctx context.Context) ([]media_items.NewMediaItemResult, error)
-}
-
-func defaultRepo(authenticatedClient *http.Client, upldr uploader.MediaUploader) media_items.HttpMediaItemsService {
-	return media_items.NewHttpMediaItemsService(authenticatedClient, upldr)
+	CreateMany(ctx context.Context, mediaItems []SimpleMediaItem) ([]MediaItem, error)
+	CreateManyToAlbum(ctx context.Context, albumId string, mediaItems []SimpleMediaItem) ([]MediaItem, error)
+	Get(ctx context.Context, itemId string) (*MediaItem, error)
+	ListByAlbum(ctx context.Context, albumId string) ([]MediaItem, error)
 }
 
 // HttpMediaItemsService implements a Google Photos client.
@@ -33,27 +27,34 @@ type HttpMediaItemsService struct {
 	repo Repository
 }
 
-// CreateFromFile create one media items after uploading the file.
-// Media item will be added to the end of the Album.
-func (s HttpMediaItemsService) CreateFromFile(ctx context.Context, albumId string, path string) (MediaItem, error) {
-	nullMediaItem := MediaItem{}
-	results, err := s.CreateManyFromFiles(ctx, albumId, []string{path})
+func (ms HttpMediaItemsService) Create(ctx context.Context, mediaItem SimpleMediaItem) (MediaItem, error) {
+	result, err := ms.CreateMany(ctx, []SimpleMediaItem{mediaItem})
 	if err != nil {
-		return nullMediaItem, err
+		return MediaItem{}, err
 	}
-	return results[0], nil
+	return result[0], nil
 }
 
-// CreateManyFromFiles create one or multiple media items after uploading the files.
-// Media items will be added to the end of the Album.
-func (s HttpMediaItemsService) CreateManyFromFiles(ctx context.Context, albumId string, paths []string) ([]MediaItem, error) {
-	mediaItems := make([]MediaItem, 0)
-	results, err := s.repo.BatchCreateItemsFromFiles(albumId, paths, albums.AlbumPosition{Position: albums.AlbumPositionTypeLastInAlbum}, ctx)
+func (ms HttpMediaItemsService) CreateMany(ctx context.Context, mediaItems []SimpleMediaItem) ([]MediaItem, error) {
+	return ms.repo.CreateMany(ctx, mediaItems)
+}
+
+func (ms HttpMediaItemsService) CreateToAlbum(ctx context.Context, albumId string, mediaItem SimpleMediaItem) (MediaItem, error) {
+	result, err := ms.CreateManyToAlbum(ctx, albumId, []SimpleMediaItem{mediaItem})
 	if err != nil {
-		return mediaItems, err
+		return MediaItem{}, err
 	}
-	for _, i := range results {
-		mediaItems = append(mediaItems, i.MediaItem)
-	}
-	return mediaItems, nil
+	return result[0], nil
+}
+
+func (ms HttpMediaItemsService) CreateManyToAlbum(ctx context.Context, albumId string, mediaItems []SimpleMediaItem) ([]MediaItem, error) {
+	return ms.repo.CreateManyToAlbum(ctx, albumId, mediaItems)
+}
+
+func (ms HttpMediaItemsService) Get(ctx context.Context, mediaItemId string) (*MediaItem, error) {
+	return ms.Get(ctx, mediaItemId)
+}
+
+func (ms HttpMediaItemsService) ListByAlbum(ctx context.Context, albumId string) ([]MediaItem, error) {
+	return ms.ListByAlbum(ctx, albumId)
 }
