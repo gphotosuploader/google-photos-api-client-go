e35ee6c8c87b8e39a4751d5c21bc980bd6882e68
pacoorozco <paco@pacoorozco.info>
2020-12-21 10:24:55 +0100

    Move Cachita test to albums_test package
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- albums/cachita_test.go
+++ albums/cachita_test.go
@@ -1,23 +1,25 @@
-package albums
+package albums_test
 
 import (
 	"context"
 	"errors"
 	"reflect"
 	"testing"
+
+	"github.com/gphotosuploader/google-photos-api-client-go/v2/albums"
 )
 
 func TestCachitaCache(t *testing.T) {
-	c := NewCachitaCache()
+	c := albums.NewCachitaCache()
 	ctx := context.Background()
 
 	// test cache miss
-	if _, err := c.GetAlbum(ctx, "nonexistent"); !errors.Is(err, ErrCacheMiss) {
-		t.Errorf("want: %v, got: %v", ErrCacheMiss, err)
+	if _, err := c.GetAlbum(ctx, "nonexistent"); !errors.Is(err, albums.ErrCacheMiss) {
+		t.Errorf("want: %v, got: %v", albums.ErrCacheMiss, err)
 	}
 
 	// test put/get
-	b1 := Album{Title: "album1"}
+	b1 := albums.Album{Title: "album1"}
 	if err := c.PutAlbum(ctx, b1); err != nil {
 		t.Fatalf("put: %v", err)
 	}
@@ -33,23 +35,23 @@ func TestCachitaCache(t *testing.T) {
 	if err := c.InvalidateAlbum(ctx, "dummy"); err != nil {
 		t.Fatalf("delete: %v", err)
 	}
-	if _, err := c.GetAlbum(ctx, "dummy"); !errors.Is(err, ErrCacheMiss) {
-		t.Errorf("want: %v, got: %v", ErrCacheMiss, err)
+	if _, err := c.GetAlbum(ctx, "dummy"); !errors.Is(err, albums.ErrCacheMiss) {
+		t.Errorf("want: %v, got: %v", albums.ErrCacheMiss, err)
 	}
 }
 
 func TestCachitaCache_PutAlbum(t *testing.T) {
 	testCases := []struct {
 		name        string
-		input       Album
+		input       albums.Album
 		errExpected bool
 	}{
-		{"empty album", Album{}, false},
-		{"album with title", Album{Title: "foo"}, false},
+		{"empty album", albums.Album{}, false},
+		{"album with title", albums.Album{Title: "foo"}, false},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			c := NewCachitaCache()
+			c := albums.NewCachitaCache()
 			err := c.PutAlbum(context.Background(), tc.input)
 			if tc.errExpected && err == nil {
 				t.Errorf("error was expected, but not produced")
@@ -72,12 +74,12 @@ func TestCachitaCache_PutManyAlbums(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			c := NewCachitaCache()
-			albums := make([]Album, len(tc.input))
+			c := albums.NewCachitaCache()
+			req := make([]albums.Album, len(tc.input))
 			for i, title := range tc.input {
-				albums[i] = Album{Title: title}
+				req[i] = albums.Album{Title: title}
 			}
-			err := c.PutManyAlbums(context.Background(), albums)
+			err := c.PutManyAlbums(context.Background(), req)
 			if tc.errExpected && err == nil {
 				t.Errorf("error was expected, but not produced")
 			}
@@ -95,17 +97,17 @@ func TestCachitaCache_GetAlbum(t *testing.T) {
 		input          string
 		errExpected    error
 	}{
-		{"empty cache", []string{}, "foo", ErrCacheMiss},
+		{"empty cache", []string{}, "foo", albums.ErrCacheMiss},
 		{"existing key", []string{"foo", "bar"}, "foo", nil},
-		{"non-existent key", []string{"foo", "bar"}, "baz", ErrCacheMiss},
+		{"non-existent key", []string{"foo", "bar"}, "baz", albums.ErrCacheMiss},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			ctx := context.Background()
-			c := NewCachitaCache()
+			c := albums.NewCachitaCache()
 			for _, title := range tc.populatedCache {
-				if err := c.PutAlbum(ctx, Album{Title: title}); err != nil {
+				if err := c.PutAlbum(ctx, albums.Album{Title: title}); err != nil {
 					t.Fatalf("error was not expected at this point. err: %s", err)
 				}
 			}
@@ -132,9 +134,9 @@ func TestCachitaCache_InvalidateAlbum(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			ctx := context.Background()
-			c := NewCachitaCache()
+			c := albums.NewCachitaCache()
 			for _, title := range populatedCache {
-				if err := c.PutAlbum(ctx, Album{Title: title}); err != nil {
+				if err := c.PutAlbum(ctx, albums.Album{Title: title}); err != nil {
 					t.Fatalf("error was not expected at this point. err: %s", err)
 				}
 			}
