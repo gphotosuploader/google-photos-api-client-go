91abf5cee6f099b077bfcd1f0d29a8b788d91d6e
pacoorozco <paco@pacoorozco.info>
2020-12-21 10:26:57 +0100

    Add assertExpectedErr  helper
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- albums/cached_service_test.go
+++ albums/cached_service_test.go
@@ -309,3 +309,12 @@ func TestNewCachedAlbumsService(t *testing.T) {
 		}
 	})
 }
+
+func assertExpectedError(errExpected bool, err error, t *testing.T) {
+	if errExpected && err == nil {
+		t.Fatalf("error was expected, but not produced")
+	}
+	if !errExpected && err != nil {
+		t.Fatalf("error was not expected, err: %s", err)
+	}
+}
