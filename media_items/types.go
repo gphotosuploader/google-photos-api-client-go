e62a0abc78f870db75f97981d9cecd2289ac2b4f
pacoorozco <paco@pacoorozco.info>
2020-12-21 14:40:52 +0100

    Document media_items/types.go
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- media_items/types.go
+++ media_items/types.go
@@ -1,10 +1,7 @@
 package media_items
 
-type SimpleMediaItem struct {
-	UploadToken string
-	FileName    string
-}
-
+// MediaItem represents of a media item (such as a photo or video) in Google Photos.
+// See: https://developers.google.com/photos/library/reference/rest/v1/mediaItems
 type MediaItem struct {
 	ID            string
 	Description   string
@@ -15,8 +12,18 @@ type MediaItem struct {
 	Filename      string
 }
 
+
+// MediaMetadata represents metadata for a media item.
+// See: https://developers.google.com/photos/library/reference/rest/v1/mediaItems
 type MediaMetadata struct {
 	CreationTime string
 	Width        string
 	Height       string
+}
+
+// SimpleMediaItem represents a simple media item to be created in Google Photos via an upload token.
+// See: https://developers.google.com/photos/library/reference/rest/v1/mediaItems/batchCreate
+type SimpleMediaItem struct {
+	UploadToken string
+	FileName    string
 }
